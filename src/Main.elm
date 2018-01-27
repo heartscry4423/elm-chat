bfbddf090364e6a462f47ffca91ed2ea9926bd13
Yoshifumi Kobuchi <kobuchi@fringe81.com>
2018-01-27 15:07:22 +0900

    TalkをListに表示するところまで

--- src/Main.elm
+++ src/Main.elm
@@ -16,6 +16,10 @@ main =
 
 
 type alias Model =
+    { talks : List Talk }
+
+
+type alias Talk =
     { imageUrl : String
     , name : String
     , message : String
@@ -29,11 +33,22 @@ type Msg
 
 init : ( Model, Cmd Msg )
 init =
-    ( { imageUrl = "https://imgcp.aacdn.jp/img-c/680/auto/tipsplus/series/246/20160608_1465380998273.jpg"
+    ( { talks = initTalks }, Cmd.none )
+
+
+initTalks : List Talk
+initTalks =
+    [ { imageUrl = "https://imgcp.aacdn.jp/img-c/680/auto/tipsplus/series/246/20160608_1465380998273.jpg"
     , name = "ダテちゃん"
     , message = "寿司食いてえ"
     , createdAt = "2018/01/27 13:00"
-    }, Cmd.none )
+    }
+    , { imageUrl = "http://www.hochi.co.jp/photo/20170718/20170718-OHT1I50084-T.jpg"
+    , name = "とみざわ"
+    , message = "ちょっと何言ってるかわかんないっす"
+    , createdAt = "2018/01/27 13:30"
+    }
+    ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -43,7 +58,7 @@ update msg model =
             ( model, Cmd.none )
 
 
-view : Model -> Html msg
+view : Model -> Html Msg
 view model =
     div [ Styles.mainWrap ]
         [ div [ Styles.postForm ]
@@ -55,32 +70,24 @@ view model =
                 , button [ Styles.postButton ] [ text "投稿！" ]
                 ]
             ]
-        , div [ Styles.talk ]
-            [ div [ Styles.talkLeft ]
-                [ img [ Styles.posterImg, src model.imageUrl ] [] ]
-            , div [ Styles.talkRight ]
-                [ div [ Styles.posterName ] [ text model.name ]
-                , div [ Styles.message ] [ text model.message ]
-                , div [ Styles.talkFooter ]
-                    [ text model.createdAt]
-                ]
-            ]
-        , div [ Styles.talk ]
-            [ div [ Styles.talkLeft ]
-                [ img [ Styles.posterImg, src "http://www.hochi.co.jp/photo/20170718/20170718-OHT1I50084-T.jpg" ] [] ]
-            , div [ Styles.talkRight ]
-                [ div [ Styles.posterName ] [ text "とみざわ" ]
-                , div [ Styles.message ] [ text "ちょっと何言ってるかわかんないっす" ]
-                , div [ Styles.talkFooter ]
-                    [ text "2018/01/27 13:30"
-                    , div [ Styles.buttons ]
-                        [ button [ Styles.editButton ] [ text "編集" ]
-                        , button [ Styles.deleteButton ] [ text "削除" ]
-                        ]
-                    ]
-                ]
+        , div [ Styles.talks ]
+            <| List.map viewTalk model.talks
+        ]
+
+
+viewTalk : Talk -> Html Msg
+viewTalk talk =
+    div [ Styles.talk ]
+        [ div [ Styles.talkLeft ]
+            [ img [ Styles.posterImg, src talk.imageUrl ] [] ]
+        , div [ Styles.talkRight ]
+            [ div [ Styles.posterName ] [ text talk.name ]
+            , div [ Styles.message ] [ text talk.message ]
+            , div [ Styles.talkFooter ]
+                [ text talk.createdAt]
             ]
         ]
+
 
 
 
