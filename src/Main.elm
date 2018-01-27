3cca4f832caf04d5714930f0544ec480bf1d89f1
Yoshifumi Kobuchi <kobuchi@fringe81.com>
2018-01-27 15:36:36 +0900

    新規投稿機能の部分まで

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Html exposing (Html, button, div, img, program, text, textarea)
 import Html.Attributes exposing (src, value)
+import Html.Events exposing (onClick, onInput)
 import Styles
 
 
@@ -16,46 +17,89 @@ main =
 
 
 type alias Model =
-    { talks : List Talk }
+    { myselfId : String
+    , members : List Member
+    , talks : List Talk
+    , field : String
+    , nextTalkIdNum : Int
+    }
+
+
+emptyMember : Member
+emptyMember =
+    { id = ""
+    , name = ""
+    , imageUrl = ""
+    }
 
 
 type alias Talk =
-    { imageUrl : String
-    , name : String
+    { id : String
+    , memberId : String
     , message : String
     , createdAt : String
     }
 
 
+type alias Member =
+    { id : String
+    , name : String
+    , imageUrl : String
+    }
+
+
 type Msg
-    = Hoge
+    = ChangeInput String
+    | Add
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { talks = initTalks }, Cmd.none )
+    ( initialModel, Cmd.none )
+
+
+initialModel : Model
+initialModel =
+    { myselfId = "m2"
+    , members = initMembers
+    , talks = initTalks
+    , field = ""
+    , nextTalkIdNum = 3
+    }
+
+
+initMembers : List Member
+initMembers =
+    [ Member "m1" "伊達ちゃん" "https://imgcp.aacdn.jp/img-c/680/auto/tipsplus/series/246/20160608_1465380998273.jpg"
+    , Member "m2" "とみざわ" "http://www.hochi.co.jp/photo/20170718/20170718-OHT1I50084-T.jpg"
+    ]
 
 
 initTalks : List Talk
 initTalks =
-    [ { imageUrl = "https://imgcp.aacdn.jp/img-c/680/auto/tipsplus/series/246/20160608_1465380998273.jpg"
-    , name = "ダテちゃん"
-    , message = "寿司食いてえ"
-    , createdAt = "2018/01/27 13:00"
-    }
-    , { imageUrl = "http://www.hochi.co.jp/photo/20170718/20170718-OHT1I50084-T.jpg"
-    , name = "とみざわ"
-    , message = "ちょっと何言ってるかわかんないっす"
-    , createdAt = "2018/01/27 13:30"
-    }
+    [ Talk "t1" "m1" "ピザ食いてえ" "2018/01/27 13:00"
+    , Talk "t2" "m2" "ちょっと何いってるかわかんないっす" "2018/01/27 13:30"
     ]
-
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Hoge ->
-            ( model, Cmd.none )
+        ChangeInput text ->
+            ( { model | field = text }, Cmd.none )
+
+        Add ->
+            let
+                nextTalkId =
+                    "t" ++ toString model.nextTalkIdNum
+
+                newTalk =
+                    Talk nextTalkId model.myselfId model.field "2018/01/27 15:30"
+            in
+            { model
+                | talks = model.talks ++ [ newTalk ]
+                , field = ""
+                , nextTalkIdNum = model.nextTalkIdNum + 1
+            } ! []
 
 
 view : Model -> Html Msg
@@ -66,22 +110,29 @@ view model =
                 [ img [ Styles.selfImg, src "http://www.hochi.co.jp/photo/20170718/20170718-OHT1I50084-T.jpg" ] []
                 ]
             , div [ Styles.formRight ]
-                [ textarea [ Styles.formArea ] []
-                , button [ Styles.postButton ] [ text "投稿！" ]
+                [ textarea [ Styles.formArea, value model.field, onInput ChangeInput ] []
+                , button [ Styles.postButton, onClick Add ] [ text "投稿！" ]
                 ]
             ]
         , div [ Styles.talks ]
-            <| List.map viewTalk model.talks
+            <| List.map (\talk -> viewTalk talk model) model.talks
         ]
 
 
-viewTalk : Talk -> Html Msg
-viewTalk talk =
+viewTalk : Talk -> Model -> Html Msg
+viewTalk talk model =
+    let
+        member =
+            model.members
+                |> List.filter (\member -> member.id == talk.memberId)
+                >> List.head
+                >> Maybe.withDefault emptyMember
+    in
     div [ Styles.talk ]
         [ div [ Styles.talkLeft ]
-            [ img [ Styles.posterImg, src talk.imageUrl ] [] ]
+            [ img [ Styles.posterImg, src member.imageUrl ] [] ]
         , div [ Styles.talkRight ]
-            [ div [ Styles.posterName ] [ text talk.name ]
+            [ div [ Styles.posterName ] [ text member.name ]
             , div [ Styles.message ] [ text talk.message ]
             , div [ Styles.talkFooter ]
                 [ text talk.createdAt]
